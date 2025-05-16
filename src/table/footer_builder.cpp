a022ccaf6f624aceb64ee0384ed9d43e52188d1b
Yifan Qu <quyifan430105@gmail.com>
2025-05-16 15:39:44 +0000

    sst

---
+++ src/table/footer_builder.cpp
@@ -0,0 +1,14 @@
+#include "footer_builder.h"
+#include "utils/codec.h"
+
+namespace smallkv {
+
+    DBStatus FooterBuilder::add(const OffsetInfo &meta_block_offset_info,
+                                const OffsetInfo &index_block_offset_info) {
+        auto first = meta_block_offset_info;
+        auto second = index_block_offset_info;
+        utils::PutFixed64(_data, *reinterpret_cast<uint64_t *>(&first));
+        utils::PutFixed64(_data, *reinterpret_cast<uint64_t *>(&second));
+        return Status::Success;
+    }
+}
